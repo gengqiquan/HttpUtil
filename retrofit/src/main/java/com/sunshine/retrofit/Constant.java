29e0bc5736e63a7d2bd4675e1fead775ecbc4901
gengqiquan <542400966@qq.com>
2016-11-23 14:13:45 +0800

    delete image

--- retrofit/src/main/java/com/sunshine/retrofit/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.sunshine.retrofit;
-
-public class Constant {
-    public static  String APP_VERSION = "";
-    public static final String KEY_USERNAME = "username";
-    public static final String NETWORK_ERROR_MSG = "网络操作失败";
-}
